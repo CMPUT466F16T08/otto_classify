afafaccf0f145cbde743edd21415573a1ab2619d
hanwang <han8@ualberta.ca>
2016-11-15 00:08:29 -0700

    Revert "add logloss"
    This reverts commit 4a9921a5700b751225bc2438f2d7662081df851b.

--- radom_forest/random_forest.py
+++ radom_forest/random_forest.py
@@ -5,26 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.calibration import CalibratedClassifierCV
-from sklearn.metrics import log_loss
 
-def predit_result(array):
-    choice = []
-    for i in range(len(array)):
-        line = array[i]
-        max_ind = 0
-        for j in range(len(line)):
-            if float(line[j]) > float(line[max_ind]):
-                max_ind = j
-        choice.append(max_ind)
-    return choice
-
-def cal_accuracy(predict, ytest):
-    correct = 0
-    for i in range(len(predict)):
-        if predict[i]==ytest[i]:
-            correct += 1
-    return float(correct)/len(predict)  
-    
 # read file, get training data
 X = pd.read_csv('train.csv')
 
@@ -42,26 +23,23 @@ X = X.drop('target', axis=1)
 # Reference: http://stackoverflow.com/questions/29438265/stratified-train-test-split-in-scikit-learn
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.20, random_state=36)
 
-forest = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_features='sqrt', min_samples_leaf=1)
+# Parameters: 
+#     Classification: 
+#         n_estimators = 100
+#         min_samples_leaf = 1
+#
+#     Calibration: 
+#         method = 'sigmoid'
 
+forest = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_features='sqrt', min_samples_leaf=1)
 forestCal = CalibratedClassifierCV(forest, method='sigmoid', cv=5)
 forestCal = forestCal.fit(Xtrain, ytrain)
-predict_prob = forestCal.predict_proba(Xtest)
-logloss = log_loss(ytest, predict_prob)
-prediction = predit_result(predict_prob)
-accuracy = cal_accuracy(prediction, ytest)
-print '\nLogloss (with calibration using sigmoid) = ' + str(logloss)
-print 'Accuracy (with calibration using sigmoid) = ' + str(accuracy)
-
-forestCal2 = CalibratedClassifierCV(forest, method='isotonic', cv=5)
-forestCal2 = forestCal2.fit(Xtrain, ytrain)
-predict_prob2 = forestCal2.predict_proba(Xtest)
-logloss2 = log_loss(ytest, predict_prob2)
-prediction = predit_result(predict_prob2)
-accuracy = cal_accuracy(prediction, ytest)
-print '\nLogloss (with calibration using isotonic) = ' + str(logloss2)
-print 'Accuracy (with calibration using isotonic) = ' + str(accuracy)
-
+predict = forestCal.predict(Xtest)
+correct = 0
+for i in range(len(predict)):
+    if predict[i]==ytest[i]:
+        correct += 1
+print '\nAccuracy (with calibration using sigmoid) = ' + str(float(correct)/len(predict))
 '''
 Choosing parameter
 '''
@@ -106,15 +84,42 @@ for nl in num_leaf_list:
 '''
 
 '''
-without calibration:
-'''
-'''
+forest = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_features='sqrt', min_samples_leaf=1)
+forest = forest.fit(Xtrain, ytrain)
+predict = forest.predict(Xtest)
+correct = 0
+for i in range(len(predict)):
+    if predict[i]==ytest[i]:
+        correct += 1
+print 'Accuracy /without bagging /without calibration= ' + str(float(correct)/len(predict))
+
+forest = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_features='sqrt', min_samples_leaf=1)
 forestBag = BaggingClassifier(forest, n_estimators=5)
 forestBag = forestBag.fit(Xtrain, ytrain)
-predict_prob0 = forestBag.predict_proba(Xtest)
-logloss0 = log_loss(ytest, predict_prob0)
-prediction = predit_result(predict_prob0)
-accuracy = cal_accuracy(prediction, ytest)
-print '\nLogloss (without calibration) = ' + str(logloss0)
-print 'Accuracy (without calibration) = ' + str(accuracy)
+predict = forestBag.predict(Xtest)
+correct = 0
+for i in range(len(predict)):
+    if predict[i]==ytest[i]:
+        correct += 1
+print '\nAccuracy /with bagging /without calibration = ' + str(float(correct)/len(predict))
+
+forest = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_features='sqrt', min_samples_leaf=1)
+forestCal = CalibratedClassifierCV(forest, method='isotonic', cv=5)
+forestCal = forestCal.fit(Xtrain, ytrain)
+predict = forestCal.predict(Xtest)
+correct = 0
+for i in range(len(predict)):
+    if predict[i]==ytest[i]:
+        correct += 1
+print '\nAccuracy /with calibration using isotonic = ' + str(float(correct)/len(predict))
+
+forest = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_features='sqrt', min_samples_leaf=1)
+forestCal = CalibratedClassifierCV(forest, method='sigmoid', cv=5)
+forestCal = forestCal.fit(Xtrain, ytrain)
+predict = forestCal.predict(Xtest)
+correct = 0
+for i in range(len(predict)):
+    if predict[i]==ytest[i]:
+        correct += 1
+print '\nAccuracy /with calibration using sigmoid = ' + str(float(correct)/len(predict))
 '''
